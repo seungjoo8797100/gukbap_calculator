1d2db6037c83b5e1d4f13e717c0165a52521df79
SeungJoo06 <seungjoo13579@gmail.com>
2022-08-15 13:40:14 +0900

    완성

--- 국밥계산기/GC.java
+++ 국밥계산기/GC.java
@@ -12,6 +12,7 @@ public class GC {
         String text = sc.next();
         System.out.println(text +"의 가격을 적어주세요");
         int food = sc.nextInt();
+        bowl = food/gukbap;
         if(gukbap > food) {
             food = gukbap - food;
             text = "그 돈이면 차라리 " +food+"원 보태서 뜨끈~하고 든든~한 국밥 한 그릇 사먹고 말지";
@@ -20,6 +21,9 @@ public class GC {
         } else {
             text = "그 돈이면 뜨끈~하고 든든~한 국밥 "+bowl+"그릇은 사먹을 수 있겠다";
         }
-        
+        if(food >= 100000000) {
+            text = "그 돈이면 차라리 국밥집을 하나 사고말지";
+        }
+       System.out.println(text);
     }
 }
